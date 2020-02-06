0dcfbc748f0a666c739fc809be925f8d0287dca4
Dylan <dcren2010@gmail.com>
2020-02-06 14:07:12 -0500

    installed persistor so that store can be saved into localstorage

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from 'react-router-dom'
-
+import {BrowserRouter as Router} from 'react-router-dom';
 import {Provider} from 'react-redux'
 
 import './index.css';
 import App from './App';
-import store from "./redux/store";
+
+import {PersistGate} from "redux-persist/integration/react";
+import {store, persistor} from './redux/store';
 
 ReactDOM.render(
     <Provider store={store}>
         <Router>
-            <App/>
+            <PersistGate persistor={persistor}>
+                <App />
+            </PersistGate>
         </Router>
     </Provider>,
     document.getElementById('root'));
